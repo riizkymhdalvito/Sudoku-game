3a8dc0f97d651b5590896fa548afccc542a88386
riizkymhdalvito <161824354+riizkymhdalvito@users.noreply.github.com>
2024-03-20 23:49:51 +0700

    GameManager.cpp

---
+++ GameManager.cpp
@@ -0,0 +1,16 @@
+#include "GameManager.h"
+#include <iostream>
+
+GameManager::GameManager(std::string playerName) : player(playerName) {}
+
+void GameManager::startGame() {
+    // Implementasi mulai permainan
+}
+
+void GameManager::chooseDifficulty() {
+    // Implementasi pilihan kesulitan
+}
+
+bool GameManager::isValidInput(int row, int col, int num) {
+    // Implementasi validasi input
+}
